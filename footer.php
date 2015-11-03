ffea379d5cd6451d0ccbd713d9b7b9db3cb5726d
Fernane Zohair <z.fernane@gmail.com>
2015-11-03 16:52:37 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,33 +1,28 @@
-<!DOCTYPE html>
-
 <footer>
-    <div class="boite_logo">
-        <img src="images/logo-bts.svg" width="280px" alt="Logo du site BTS-SIO.fr">
+    <div class="boite_logo_bas">
+        <img src="images/white-logo.svg" width="200px" alt="Logo du site BTS-SIO.fr">
     </div>
-            
     <div class="boite_principale">
         <div class="boite_une">
-            <h4 class="titre">Titre</h4>
-            <p class="paragraphe">TEXTE 1</p>
+            <h4 class="titre">LONG TITLE OF WIDGETS</h4>
+            <p class="paragraphe">Donec facilisis tortor a ligula dignissim, eget tempor tortor convallis. Quisque lacinia sit amet mi quis dignissim. Vivamus sed malesuada nisi, sed semper sapien. Quisque lobortis, metus ut ultrices maximus, ipsum justo ornare mauris, a suscipit dui lorem sit amet nisi. Vestibulum sed velit eget nulla congue tempor.</p>
         </div>
-
         <div class="boite_deux">
-            <h4 class="titre">Titre</h4>
-            <p class="paragraphe">TEXTE 2</p>
-        </div>
-
+            <h4 class="titre">LONG TITLE OF WIDGETS</h4>
+            <p class="paragraphe">Donec facilisis tortor a ligula dignissim, eget tempor tortor convallis. Quisque lacinia sit amet mi quis dignissim. Vivamus sed malesuada nisi, sed semper sapien. Quisque lobortis, metus ut ultrices maximus, ipsum justo ornare mauris, a suscipit dui lorem sit amet nisi. Vestibulum sed velit eget nulla congue tempor.</p>
+            </div>
         <div class="boite_trois">
-            <h4 class="titre">Titre</h4>
-            <p class="paragraphe">TEXTE 3</p>
+            <h4 class="titre">LONG TITLE OF WIDGETS</h4>
+            <p class="paragraphe">Donec facilisis tortor a ligula dignissim, eget tempor tortor convallis. Quisque lacinia sit amet mi quis dignissim. Vivamus sed malesuada nisi, sed semper sapien. Quisque lobortis, metus ut ultrices maximus, ipsum justo ornare mauris, a suscipit dui lorem sit amet nisi. Vestibulum sed velit eget nulla congue tempor.</p>
         </div>
-
         <div class="boite_quatre">
-            <h4 class="titre">Titre</h4>
-            <p class="paragraphe">TEXTE 4</p>
+            <h4 class="titre">LONG TITLE OF WIDGETS</h4>
+            <p class="paragraphe">Donec facilisis tortor a ligula dignissim, eget tempor tortor convallis. Quisque lacinia sit amet mi quis dignissim. Vivamus sed malesuada nisi, sed semper sapien. Quisque lobortis, metus ut ultrices maximus, ipsum justo ornare mauris, a suscipit dui lorem sit amet nisi. Vestibulum sed velit eget nulla congue tempor.</p>
         </div>
     </div>
-                
     <div class="copyright">
-        <p>Copyright à compléter</p>
-    </div>
-</footer>
+        <p>Copyright © 2015 | Développer par Zed'Creative</p>
+        </div>
+        </footer>
+</body>
+</html>
