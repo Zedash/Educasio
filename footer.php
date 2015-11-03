2fa29f247a8e856b6a9f43d9c4a6f8307ce177d0
Quentin771 <Quentiin.p.ecole@gmail.com>
2015-11-03 15:57:33 +0100

    Ajout du footer

---
+++ footer.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Zed'Ash">
+        <meta name="description" content="Site internet du BTS SIO baser sur le thème Educasio© par les étudiant d'Itis Formation.">
+        <meta name="keywords" content="BTS,SIO,ITIS,FORMATION,EDUCASIO,ETUDIANT">
+        <link type="text/css" rel="stylesheet" href="style.css">
+        <link type="text/css" rel="stylesheet" href="css/font-awesome.min.css">
+        <title><!--EMPLACEMENT DU CODE PHP POUR RECUPERER LE TITRE DE LA PAGE WORDPRESS--></title>
+    </head>
+    
+    <body>
+        <footer>
+                <div class="boite_logo">
+                    <img src="images/logo-bts.svg" width="280px" alt="Logo du site BTS-SIO.fr">
+                </div>
+            
+                <div class="boite_principale">
+                    <div class="boite_une">
+                        <h4 class="titre">Titre</h4>
+                        <p class="paragraphe">TEXTE 1</p>
+                    </div>
+
+                    <div class="boite_deux">
+                        <h4 class="titre">Titre</h4>
+                        <p class="paragraphe">TEXTE 2<p>
+                    </div>
+
+                    <div class="boite_trois">
+                        <h4 class="titre">Titre</h4>
+                        <p class="paragraphe">TEXTE 3<p>
+                    </div>
+
+                    <div class="boite_quatre">
+                        <h4 class="titre">Titre</h4>
+                        <p class="paragraphe">TEXTE 4<p>
+                    </div>
+                </div>
+                    
+                <div class="copyright">
+                    <p>Copyright à compléter</p>
+                </div>
+        </footer>
+    </body>
